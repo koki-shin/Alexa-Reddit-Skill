a9f061bf8707fe809dbc350bea1e8284d1a5dd2b
kazuki-shin <kazukis2@illinois.edu>
2019-03-22 23:51:03 -0500

    finished and tested nba reddit alexa skill

---
+++ redditreader.py
@@ -0,0 +1,47 @@
+from flask import Flask
+from flask_ask import Ask, statement, question, session
+import json
+import requests
+import time
+import unidecode
+
+app = Flask(__name__)
+ask = Ask(app, "/reddit_reader")
+
+def get_headlines():
+    user_pass_dict = {'user': 'koki_shin',
+                      'passwd': 'kazunobu1212',
+                      'api_type': 'json'}
+    sess = requests.Session()
+    sess.headers.update({'User-Agent': 'I am testing Alexa: koki'})
+    sess.post('https://www.reddit.com/api/login', data = user_pass_dict)
+    time.sleep(1)
+    url = 'https://reddit.com/r/nba/.json?limit=10'
+    html = sess.get(url)
+    data = json.loads(html.content.decode('utf-8'))
+    titles = [unidecode.unidecode(listing['data']['title']) for listing in data['data']['children']]
+    titles = '... '.join([i for i in titles])
+    return titles
+
+@app.route('/')
+def homepage():
+    return "hi there, how ya doin?"
+
+@ask.launch
+def start_skill():
+    welcome_message = 'Hello there, would you like the news?'
+    return question(welcome_message)
+
+@ask.intent("YesIntent")
+def share_headlines():
+    headlines = get_headlines()
+    headline_msg = 'Hey Koki the current NBA news headlines are {}'.format(headlines)
+    return statement(headline_msg)
+
+@ask.intent("NoIntent")
+def no_intent():
+    bye_text = 'I am not sure why you asked me to run then, but okay... bye'
+    return statement(bye_text)
+
+if __name__ == '__main__':
+    app.run(debug=True)
